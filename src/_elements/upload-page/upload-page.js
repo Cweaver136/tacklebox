e9e40d2d59bd7333d04eba5d407fab57dba6740a
Caleb Weaver <cweaver136@gmail.com>
2020-10-31 16:42:01 -0400

    reworked photos, and cleaned up upload page

--- src/_elements/upload-page/upload-page.js
+++ src/_elements/upload-page/upload-page.js
@@ -67,6 +67,16 @@ class UploadPage extends PolymerElement {
       <tacklebox-toolbar user="{{user}}"></tacklebox-toolbar>
       <login-element hidden\$="[[hasUser(user.*)]]" user="{{user}}"></login-element>
       <template is="dom-if" if="{{user}}">
+      <paper-toast id="progressToast" style="min-width:315px;" visible="false" duration="0" no-cancel-on-esc-key="">
+        <div id="progressText" style="margin-bottom:3px;">{{progressText}}</div>
+        <paper-progress id="progressPercent" style="width:auto;" value="{{progressPercent}}"></paper-progress>
+      </paper-toast>
+      <paper-toast id="toast" text="{{toastText}}" visible="false" duration="{{toastDuration}}">
+        <span id="toastBody"></span>
+        <template is="dom-if" if="{{showCloseToastButton}}">
+          <paper-button style="margin:5px; padding:5px;" on-tap="closeToast">Close</paper-button>
+        </template>
+      </paper-toast>
         <div id="content" style="flex: 1">
           <div hidden\$="{{!equal(pageIndex, 0)}}" index="0" class="pageView flex-col-center-h">
             <h2>Pick your fishing trip</h2>
@@ -77,17 +87,29 @@ class UploadPage extends PolymerElement {
                 </template>
               </paper-listbox>
             </paper-dropdown-menu>
-            <paper-button disabled="{{!canContinue}}" class="button" on-tap="changePage">Continue</paper-button>
+            <paper-button disabled="{{!canContinue}}" class="button" page="1" on-tap="changePage">Continue</paper-button>
           </div>
           <div hidden\$="{{!equal(pageIndex, 1)}}" index="1" class="pageView flex-col-center-h">
             <h2>{{computeSessionTitle(selectedSession)}}</h2>
             <paper-button class="button uploadButton" on-tap="selectPhotos">Select Photos</paper-button>
             <input style="display: none" type="file" id="files" multiple />
             <div class="flex-row-center" id="photoContainer">
+              <template is="dom-repeat" items="{{toArray(photosToUpload)}}" as="picture">
+                <div style="height: 135px; position: relative; margin: 5px">
+                  <img src="{{picture.src}}" class="thumbnailPhoto">
+                  <paper-icon-button icon="icons:delete" key\$="{{picture.$key}}" on-tap="deleteThumbnail" class="thumbnailDelete"></paper-icon-button>
+                </div>
+              </template>
             </div>
             <div class="flex-row-center">
-              <paper-button style="background-color: #272932" on-tap="changePage" class="button">Back</paper-button>
+              <paper-button style="background-color: #272932" page="0" on-tap="changePage" class="button">Back</paper-button>
               <paper-button on-tap="uploadPhotos" class="button">Upload Photos</paper-button>
+            </div>
+          </div>
+          <div hidden\$="{{!equal(pageIndex, 2)}}" index="2" class="pageView flex-col-center-h">
+            <div class="flex-col-center-vh">
+              <span>{{numPhotosUploaded}} Photos have successfully been uploaded to {{computeSessionTitle(selectedSession)}}</span>
+              <paper-button on-tap="changePage" page="0" class="button">Upload More</paper-button>
             </div>
           </div>
         </div>
@@ -116,7 +138,7 @@ class UploadPage extends PolymerElement {
       },
       photosToUpload: {
         type: Array,
-        value: [],
+        value: {},
       }
     };
   }
@@ -126,45 +148,87 @@ class UploadPage extends PolymerElement {
       'calcCanContinue(selectedSessionIndex)'
     ]
   }
+  ready() {
+    super.ready();
+  }
   selectPhotos() { 
     this.shadowRoot.querySelector('#files').addEventListener('change', e => {
       var files = e.target.files;
+      let count = files.length;
+      let obj = {};
+      this.toast(`Loading Photos...`, null, 100000000);
       // Loop through the FileList and render image files as thumbnails.
-      for (var i = 0; i < files.length; i++) {
+      for (var i = 0; i < count; i++) {
         let file = files[i];
         // Only process image files.
-        if (!file.type.match('image.*')) {
-          continue;
+        if (file.type.match('image.*')) {
+          var reader = new FileReader();
+          // Closure to capture the file information.
+          reader.onload = (event) => {
+              // Render thumbnail.
+              file.src = event.target.result
+              // Add file to photosToUpload array
+              if (!this.photosToUpload[file.name]) obj[file.name] = file
+              if (!--count) this.filesLoaded(obj);
+          };
+          // Read in the image file as a data URL.
+          reader.readAsDataURL(file);
         }
-
-        var reader = new FileReader();
-        // Closure to capture the file information.
-        reader.onload = event => {
-            // Render thumbnail.
-            var div = document.createElement('div');
-            div.style = `height: 135px; position: relative; margin: 5px;`;
-            var img = document.createElement('img');
-            img.src = event.target.result;
-            img.classList.add('thumbnailPhoto');
-            div.appendChild(img);
-            div.innerHTML += `<paper-icon-button icon="icons:delete" class="thumbnailDelete" on-tap="deleteThumbnail"></paper-icon-button>`
-            this.shadowRoot.querySelector('#photoContainer').insertBefore(div, null);
-        };
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(file);
-
-        // Add file to photosToUpload array
-        this.push('photosToUpload', file);
       }
     }, false);
     this.shadowRoot.querySelector('#files').click();
   }
-  deleteThumbnail() {
-    console.log("deleting")
+  async uploadPhotos() {
+    let filesLength = Object.keys(this.photosToUpload).length;
+    this.numPhotosUploaded = filesLength;
+    if (filesLength > 0) {
+      this.shadowRoot.querySelector('#progressToast').open();
+      this.shadowRoot.querySelector('#progressPercent').indeterminate = true;
+      this.progressPercent = 0;
+      let update = {};
+      let promises = [];
+      let counter = 0;
+      this.progressText = `Uploading Photo ${counter + 1} of ${filesLength}`;
+      for (let key in this.photosToUpload) {
+        let photo = this.photosToUpload[key];
+        let photoId = uuidv4();;
+        promises.push(new Promise(resolve => {
+          let pictureRef = firebase.storage().ref(`photos/${this.selectedSession.$key}/${photoId}`)
+          pictureRef.put(photo).then(() => {
+            pictureRef.getDownloadURL().then(url => {
+              update[this.selectedSession.$key + '/photos/'+ photoId] = {url};
+              this.progressPercent = (++counter / filesLength) * 100;
+              this.progressText = `Uploading Photo ${counter} of ${filesLength}`;
+              resolve();
+            })
+          });
+        }))
+      }
+      Promise.all(promises).then(() => {
+        firebase.database().ref('sessions').update(update).then(() => {
+          this.progressText = "Photos Uploaded Complete";
+          this.shadowRoot.querySelector('#progressPercent').indeterminate = false;
+          this.progressPercent = 100;
+          this.pageIndex = 2;
+          this.photosToUpload = {};
+          setTimeout(() => this.shadowRoot.querySelector('#progressToast').close(), 5000);
+        })
+      })
+    }
+    else {
+      this.toast('No photos selected to upload')
+    }
   }
-  ready() {
-    super.ready();
+  filesLoaded(files) {
+    this.set('photosToUpload', files);
+    this.toast(`Loading Photos...`, null, 2000);
+  } 
+  deleteThumbnail(e) {
+    let key = e.target.getAttribute('key');
+    let temp = this.photosToUpload;
+    delete temp[key];
+    console.log("temp", temp)
+    this.set('photosToUpload', temp)
   }
   hasUser() {
     if (this.user) return true;
@@ -179,8 +243,8 @@ class UploadPage extends PolymerElement {
     }
   }
   changePage(e) {
-    if (this.pageIndex == 1) this.pageIndex = 0;
-    else if (this.pageIndex == 0) this.pageIndex = 1;
+    let index = e.target.getAttribute('page');
+    this.pageIndex = index
   }
   computeSessionTitle(item) {
     return `${item.body_of_water} - ${moment(item.date).format('M[/]D')}`
@@ -203,32 +267,17 @@ class UploadPage extends PolymerElement {
       this.set('fishingSessions', k.toArray(fishingSessions))
     }
   }
-  async uploadPhotos() {
-    if (this.photosToUpload.length > 0) {
-      let update = {};
-      let promises = [];
-      for (let i = 0; i < this.photosToUpload.length; i++) {
-        let photoId = uuidv4();;
-        promises.push(new Promise(resolve => {
-          // todo get photo data and save it to firebase
-          let pictureRef = firebase.storage().ref(`photos/${this.selectedSession.$key}/${photoId}`)
-          pictureRef.put(this.photosToUpload[i]).then(() => {
-            pictureRef.getDownloadURL().then(url => {
-              update[this.selectedSession.$key + '/photos/'+ photoId] = {url};
-              resolve();
-            })
-          });
-        }))
-      }
-      Promise.all(promises).then(() => {
-        firebase.database().ref('sessions').update(update).then(() => {
-          console.log("photos uploaded successfully");
-        })
-      })
+  toast(text, innerHTML, duration) {
+    this.shadowRoot.querySelector('#toast').hide();
+    this.toastText = text;
+    if (innerHTML != null) {
+      this.shadowRoot.querySelector('#toastBody').innerHTML = innerHTML;
+    } else {
+      this.set('$.toastBody.innerHTML', '');
     }
-    // else {
-    //   this.toast('No photos selected to upload')
-    // }
+    if (duration != null) this.set('toastDuration', duration);
+    else this.set('toastDuration', 6000);
+    this.shadowRoot.querySelector('#toast').show();
   }
 }
 customElements.define(UploadPage.is, UploadPage);
--- src/helpers/polymer-imports.js
+++ src/helpers/polymer-imports.js
@@ -4,3 +4,5 @@ import '@polymer/paper-item/paper-item.js';
 import '@polymer/paper-listbox/paper-listbox.js';
 import '@polymer/paper-button/paper-button.js'
 import '@polymer/paper-dialog/paper-dialog.js'
+import '@polymer/paper-toast/paper-toast.js'
+import '@polymer/paper-progress/paper-progress.js'
